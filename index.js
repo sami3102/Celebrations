67c1b40ebcbb7a7ba43224e435bd7e1e6bf76eda
Mohammed Sami Pasha <97223241+sami3102@users.noreply.github.com>
2024-06-08 03:05:13 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,6 @@
+var bt = document.getElementsByClassName("burg-icon")[0];
+bt.addEventListener("click",function () {
+    var me = document.getElementById("burg-menu");
+    me.classList.toggle("off");
+
+  })
